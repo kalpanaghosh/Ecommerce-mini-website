66426018116f28607101f626afaf7382f97e98b4
kalpanaghosh <121330748+kalpanaghosh@users.noreply.github.com>
2026-05-09 16:12:13 +0530

    fixed cors issue

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 
 // Database Connection
